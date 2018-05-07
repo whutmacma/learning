0fc9cc3e3f3f7220224bf9faf898d13019b00bba
mac <whutmachangcong@sina.com>
2018-05-08 00:17:49 +0800

    filter is starting

--- apps/organization/views.py
+++ apps/organization/views.py
@@ -14,17 +14,23 @@ class OrganizationView(View):
     def get(self, request):
         all_organizations = CourseOrg.objects.all()
         all_citys = CityDict.objects.all()
+
+        city_id = request.GET.get('city', '')
+        if city_id:
+            all_organizations =  all_organizations.filter(city_id=int(city_id))
+
         try:
             page = request.GET.get('page', 1)
         except PageNotAnInteger:
             page = 1
 
-        p = Paginator(all_organizations, request=request)
+        p = Paginator(all_organizations, 1, request=request)
         orgs = p.page(page)
 
         return render(request, "org-list.html", {
             "all_organizations":orgs,
-            "all_citys":all_citys
+            "all_citys":all_citys,
+            "city_id":city_id
         })
 
 
